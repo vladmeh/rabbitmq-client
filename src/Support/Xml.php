b14adb72a0785a20c0f1fa8b5fcab7e3b1bc4af2
Mikhaylov.V <mihailov.v@fhclub.ru>
2020-10-26 12:59:00 +0300

    refactor Support\Xml toArray && toJson

--- src/Support/Xml.php
+++ src/Support/Xml.php
@@ -44,35 +44,55 @@ class Xml
      */
     public static function toJson($xml)
     {
-        return json_encode(static::toArray($xml), JSON_UNESCAPED_UNICODE);
+        return json_encode(self::toArray($xml), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
     }
 
     /**
      * @param $data
      * @param null $result
+     * @param int $recursionDepth
      * @return array|null
      */
-    public static function toArray($data, &$result = null)
+    public static function toArray($data, &$result = null, &$recursionDepth = 0)
     {
         if (is_object($data)) {
+            if ($recursionDepth == 0) {
+                $callerProviderObject = $data;
+            }
             $data = get_object_vars($data);
         }
 
         if (is_array($data)) {
             foreach ($data as $key => $value) {
                 $res = null;
-                static::toArray($value, $res);
+
+                $recursionDepth++;
+                self::toArray($value, $res, $recursionDepth);
+                $recursionDepth--;
+
                 if ($key === '@attributes' && ($key) || is_array($value)) {
                     $result = $res;
                 } else {
                     $result[strtolower($key)] = $res;
                 }
             }
+
+            if ($recursionDepth == 0) {
+                $temp = $result;
+                $result = [];
+                if (isset($callerProviderObject)) {
+                    $result[strtolower($callerProviderObject->getName())] = $temp;
+                } elseif (is_array($temp) && count($temp) == 1 && array_key_exists(0, $temp)) {
+                    $result = $temp[array_key_first($temp)];
+                } else {
+                    $result = $temp;
+                }
+            }
+
         } else {
             $result = $data;
         }
 
         return $result;
     }
-
 }
